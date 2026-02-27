45e315e0ca08c2b70a55faf4fb21dd9de0f6f4a9
Shekh Mohibur Rahman <119482264+shekhmohibur@users.noreply.github.com>
2026-02-27 11:32:55 +0600

    added env local

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -2,14 +2,14 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDwCpxQp9gKCmCurgj4wRbn1qu-RTEY2bg",
-  authDomain: "base-auth-1.firebaseapp.com",
-  projectId: "base-auth-1",
-  storageBucket: "base-auth-1.firebasestorage.app",
-  messagingSenderId: "718951217710",
-  appId: "1:718951217710:web:8587f9867259ed3455bdbf",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
-export default auth
+export default auth;
